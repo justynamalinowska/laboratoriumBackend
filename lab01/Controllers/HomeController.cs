02b505a95a27d2cbb34d1f5ac8ec77990812310c
justyna.malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2024-02-28 10:31:40 +0100

    Init commit

---
+++ lab01/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lab01.Models;
+
+namespace lab01.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public string Search(string q, int counter)
+    {
+        return $"q={q}, counter{counter}";
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
